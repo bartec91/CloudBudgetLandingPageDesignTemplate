5b0e8fdb2551174197e84b045cf1f6e5b0007a68
bartec91 <bartlomiej@bakalarz.net.pl>
2021-04-02 13:31:38 +0200

    Correct  Scroll Section

--- src/js/app/scripts.js
+++ src/js/app/scripts.js
@@ -1,4 +1,15 @@
+(() => {
+    /* Toggle Nav JavaScript */
+    const mainNav = document.getElementById('navbarToggle');
+    const navToggle = document.getElementById('btnToggle');
 
+    let mainNavToggle = () => {
+        mainNav.classList.toggle('collapse');
+    };
+
+    // Add a click event to run the mainNavToggle function
+    navToggle.addEventListener('click', mainNavToggle);
+})();;
 document.querySelectorAll('nav li.nav__item a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
