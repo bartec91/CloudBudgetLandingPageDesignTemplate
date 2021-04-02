95cfa69c165337ed1cd5f62a76f04b3384b498e1
bartec91 <bartlomiej@bakalarz.net.pl>
2021-04-02 17:07:43 +0200

    Correct JS Files

--- src/js/app/form-validation.js
+++ src/js/app/form-validation.js
@@ -1,66 +1,62 @@
-(() => {
+const form = document.querySelector(".section-contact__form"),
+    fields = form.querySelectorAll("[data-error]");
 
-    const form = document.querySelector(".section-contact__form"),
-        fields = form.querySelectorAll("[data-error]");
+const isNotEmpty = (field) => {
+    return field.value !== "";
+};
 
-    const isNotEmpty = (field) => {
-        return field.value !== "";
-    };
+const isPhone = (field) => {
+    return field.value.match(/^[0-9\+]{8,13}$/);
+};
 
-    const isPhone = (field) => {
-        return field.value.match(/^[0-9\+]{8,13}$/);
-    };
+const displayErrors = (errors) => {
+    let ul = document.querySelector("ul.errors");
 
-    const displayErrors = (errors) => {
-        let ul = document.querySelector("ul.errors");
+    if (!ul) {
+        ul = document.createElement("ul");
 
-        if (!ul) {
-            ul = document.createElement("ul");
+        ul.classList.add("errors");
+    }
 
-            ul.classList.add("errors");
+    ul.innerHTML = "";
+
+    errors.forEach((error) => {
+        let li = document.createElement("li");
+
+        li.textContent = error;
+
+        ul.appendChild(li);
+    });
+
+    form.parentNode.insertBefore(ul, form);
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let errors = [];
+
+    for (let i = 0; i < fields.length; i++) {
+        let field = fields[i],
+            isValid = false;
+
+        if (field.type === "text") {
+            isValid = isNotEmpty(field);
+        } else if (field.type === "tel") {
+            isValid = isPhone(field);
         }
 
-        ul.innerHTML = "";
-
-        errors.forEach((error) => {
-            let li = document.createElement("li");
-
-            li.textContent = error;
-
-            ul.appendChild(li);
-        });
-
-        form.parentNode.insertBefore(ul, form);
-    };
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        let errors = [];
-
-        for (let i = 0; i < fields.length; i++) {
-            let field = fields[i],
-                isValid = false;
-
-            if (field.type === "text") {
-                isValid = isNotEmpty(field);
-            } else if (field.type === "tel") {
-                isValid = isPhone(field);
-            }
-
-            if (!isValid) {
-                field.classList.add("error");
-                errors.push(field.dataset.error);
-            } else {
-                field.classList.remove("error");
-            }
-        }
-
-        if (errors.length) {
-            displayErrors(errors);
+        if (!isValid) {
+            field.classList.add("error");
+            errors.push(field.dataset.error);
         } else {
-            form.submit();
+            field.classList.remove("error");
         }
-    }, false);
+    }
 
-})();
+    if (errors.length) {
+        displayErrors(errors);
+    } else {
+        form.submit();
+    }
+}, false);
